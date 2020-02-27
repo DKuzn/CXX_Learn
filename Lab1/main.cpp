1751b5aafeeb7b6c1570bf6af1c373e69e392655
DKuzn <58743394+DKuzn@users.noreply.github.com>
2020-02-27 19:41:16 +0300

    Add files via upload

---
+++ Lab1/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+int treagle(int n)
+{
+   int m = (n+1)/2;
+   char space = ' ';
+   for (int i = 1; i <= m; i++) {
+       cout.fill(' ');
+       cout << setw(m-i+1) << space;
+       cout.fill('*');
+       cout << setw(i+1) << '\n';
+   }
+   for (int i = m-1; i >= 1; i--) {
+       cout.fill(' ');
+       cout << setw(m-i+1) << space;
+       cout.fill('*');
+       cout << setw(i+1) << '\n';
+   }
+   return 1;
+}
+int main()
+{
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+    try
+    {
+        if (n == 0 or n % 2 == 0)
+        {
+            throw 1;
+        }
+        else {
+            treagle(n);
+        }
+
+    }
+    catch (int)
+    {
+        cout << "Число n должно быть нечетным и больше нуля." << "\n";
+    }
+    return 0;
+}
