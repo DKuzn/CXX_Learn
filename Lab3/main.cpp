542771030db451b76a98be3c4bf717096e0a6d80
Dmitry Kuznetsov <58743394+DKuzn@users.noreply.github.com>
2020-04-24 16:57:21 +0300

    Add files via upload

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -3,78 +3,84 @@
 #include <string>
 #include <cstdlib>
 #include <random>
+#include <vector>
 
-using namespace std;
-
-int *ArrayInit(int n)
+template<typename T>
+std::vector<T> slice(std::vector<T> &v, unsigned long m, unsigned long n)
 {
-    random_device rd;
-    mt19937 mersenne(rd());
-    int * array = new int [n];
+    std::vector<T> vec;
+    std::copy(v.begin() + m, v.begin() + n, std::back_inserter(vec));
+    return vec;
+}
+
+std::vector<unsigned long> ArrayInit(int n)
+{
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
+    std::vector<unsigned long> array = {};
     for (int i = 0; i < n; i++) {
-        array[i] = 1 + mersenne() % 100;
+        array.push_back(1 + mersenne() % 100);
     }
     return array;
 }
 
-void OutputArray(int *array, int n)
+void OutputArray(std::vector<unsigned long> array)
 {
-    for (int i = 0; i < n; i++) {
-        cout << setw(3) << array[i] << " ";
+    unsigned long n = array.size();
+    for (unsigned long i = 0; i < n; i++) {
+        std::cout << std::setw(3) << array[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
-int *Merge(int *array, int n)
+std::vector<unsigned long> Merge(std::vector<unsigned long> array)
 {
-    int *full = new int[n];
-    int m = n / 2;
-    int *L = new int[m];
-    int *R = new int[m];
-    for (int i = 0; i < m; i++){
-        L[i] = array[i];
+    unsigned long n = array.size();
+    unsigned long m = n / 2;
+    std::vector<unsigned long> full = {};
+    std::vector<unsigned long> L = {};
+    std::vector<unsigned long> R = {};
+    for (unsigned long j = 0; j < n; j += 2){
+        L.push_back(array[j]);
     }
-    for (int j = m; j < n; j++){
-        R[j] = array[j];
+    for (unsigned long k = 1; k < n; k += 2){
+        R.push_back(array[k]);
     }
-    int i = 0;
-    int j = m;
-    int k = 0;
-    while (i < m && j < n){
-        if (L[i] < R[j]){
-            full[k] = L[i];
-            full[k + 1] = R[j];
-            i++;
-            j++;
-            k = k + 2;
+    unsigned long i = 0;
+    while (i < m){
+        if (L[i] < R[i]){
+            full.push_back(L[i]);
+            full.push_back(R[i]);
         }
-        else {
-            full[k] = R[j];
-            full[k + 1] = L[i];
-            i++;
-            j++;
-            k = k + 2;
+        else if (L[i] >= R[i]){
+            full.push_back(R[i]);
+            full.push_back(L[i]);
         }
+        i++;
         }
+    if (L.size() < R.size()){
+        full.push_back(R[m]);
+    }
     return full;
 }
 
-int *MergeSort(int *array, int n)
+std::vector<unsigned long> MergeSort(std::vector<unsigned long> array)
 {
-    int *sarr = Merge(array, n);
+    std::vector<unsigned long> sarr = Merge(array);
     return sarr;
 }
 
 int main()
 {
-    int n = 12;
-    int *array;
-    array = ArrayInit(n);
-    cout << "Unsorted array:" << "\n";
-    OutputArray(array, n);
-    int *sarray = MergeSort(array, n);
-    cout << "Sorted array:" << "\n";
-    OutputArray(sarray, n);
+    int n;
+    std::cout << "Введите длину массива: " << "\n";
+    std::cin >> n;
+    std::vector<unsigned long> array = ArrayInit(n);
+    std::cout << "Unsorted array:" << "\n";
+    OutputArray(array);
+    std::vector<unsigned long> sarray = MergeSort(array);
+    std::cout << "Sorted array:" << "\n";
+    OutputArray(sarray);
     return 0;
 
 }
