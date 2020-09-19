d1fc025ea162d80abb1213424e5d16595250e37a
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-09-19 23:00:05 +0300

    update 2nd lab

--- Advanced_labs/base_classes.cpp
+++ Advanced_labs/base_classes.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 
-class Polinom
+class Polynomial
 {
 private:
     double args[3]{};
+    double roots[2]{};
 
-public: Polinom(double a, double b, double c){
+public: Polynomial(double a, double b, double c){
     args[0] = a;
     args[1] = b;
     args[2] = c;
@@ -17,24 +17,57 @@ private: double * get_args(){
         return args;
     }
 
-public: Polinom addition(Polinom polinom){
-        double * to_addition = polinom.get_args();
+public: double * get_roots(){
+        calculate();
+        return roots;
+    }
+
+public: double value(double x){
+        // Calculating polynomial value
+        double a = args[0];
+        double b = args[1];
+        double c = args[2];
+        double value = a * pow(x, 2) + b * x + c;
+        return value;
+    }
+
+private: void calculate(){
+        // Calculating argument when polynomial value equal to zero (quadratic equation)
+        double a = args[0];
+        double b = args[1];
+        double c = args[2];
+        double d = pow(b, 2) - 4 * a * c;
+        roots[0] = (-b + sqrt(d)) / (2 * a);
+        roots[1] = (-b - sqrt(d)) / (2 * a);
+    }
+
+public: void calculation(){
+        // Output roots of quadratic equation
+        calculate();
+        std::cout << "x1 = " << roots[0] << ", " << "x2 = " << roots[1] <<std::endl;
+    }
+
+public: Polynomial addition(Polynomial polynom){
+        // Addition operation for two Polynomial instance
+        double * to_addition = polynom.get_args();
         to_addition[0] += args[0];
         to_addition[1] += args[1];
         to_addition[2] += args[2];
         return {to_addition[0], to_addition[1], to_addition[2]};
     }
 
-public: Polinom subtraction(Polinom polinom){
-        double * to_subtraction = polinom.get_args();
+public: Polynomial subtraction(Polynomial polynom){
+        // Subtraction operation for two Polynomial instance
+        double * to_subtraction = polynom.get_args();
         to_subtraction[0] += -args[0];
         to_subtraction[1] += -args[1];
         to_subtraction[2] += -args[2];
         return {to_subtraction[0], to_subtraction[1], to_subtraction[2]};
     }
 
-public: Polinom multiplication(Polinom polinom){
-        double * to_multiplication = polinom.get_args();
+public: Polynomial multiplication(Polynomial polynom){
+        // Multiplication operation for two Polynomial instance
+        double * to_multiplication = polynom.get_args();
         to_multiplication[0] *= args[0];
         to_multiplication[1] *= args[1];
         to_multiplication[2] *= args[2];
@@ -42,6 +75,7 @@ public: Polinom multiplication(Polinom polinom){
     }
 
 public: void output(){
+        // Output polynomial description
         double a = args[0];
         double b = args[1];
         double c = args[2];
@@ -57,12 +91,11 @@ public: void output(){
         }
 
         if (c < 0){
-            out += std::to_string(c);
+            out += " - " + std::to_string(-c);
         }
         else {
             out += " + " + std::to_string(c);
         }
-
 
         std::cout << out << std::endl;
     }
@@ -71,14 +104,17 @@ public: void output(){
 
 int main()
 {
-    Polinom polinom1 = Polinom(1, -70, 600);
-    Polinom polinom2 = Polinom(1, -70, 600);
-    Polinom addit = polinom1.addition(polinom2);
-    Polinom multi = polinom1.multiplication(polinom2);
-    Polinom subtr = polinom1.subtraction(polinom2);
-    polinom1.output();
-    polinom2.output();
+    Polynomial polynom1 = Polynomial(1, -70, 600);
+    Polynomial polynom2 = Polynomial(1, -70, 500);
+    Polynomial addit = polynom1.addition(polynom2);
+    Polynomial multi = polynom1.multiplication(polynom2);
+    Polynomial subtr = polynom1.subtraction(polynom2);
+    polynom1.output();
+    polynom2.output();
     addit.output();
     multi.output();
     subtr.output();
+    polynom2.calculation();
+    double value = polynom2.value(polynom2.get_roots()[1]);
+    std::cout << value << std::endl;
 }
