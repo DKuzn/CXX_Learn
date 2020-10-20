93b250443fc834ef99e5cf053110355fdec300aa
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-10-20 13:54:49 +0300

    update 4th lab

--- Advanced_labs/double_matrix.cpp
+++ Advanced_labs/double_matrix.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <iomanip>
+#include <utility>
 #include <vector>
 #include <random>
 
 
-class DoubleMatrix
-{
+class MatrixException : public std::exception {
+private:
+    std::string message_error;
+
+public:
+    explicit MatrixException(std::string error) : message_error(std::move(error)) {}
+
+    const char *what() const noexcept override {
+        return message_error.c_str();
+    }
+};
+
+
+class DoubleMatrix {
 private:
     std::vector<std::vector<double>> doubleMatrix;
 
 public:
-    DoubleMatrix(int rows, int columns) {
+    int rows;
+    int columns;
+
+public:
+    DoubleMatrix(int rows_i, int columns_i) {
+        rows = rows_i;
+        columns = columns_i;
         std::random_device rd;
         std::mt19937 generator(rd());
-        std::vector <std::vector<double>> matrix(rows, std::vector<double>(columns));
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                matrix[i][j] = generator() % 100;
+        std::vector<std::vector<double>> matrix(rows_i, std::vector<double>(columns_i));
+        for (int i = 0; i < rows_i; i++) {
+            for (int j = 0; j < columns_i; j++) {
+                matrix[i][j] = generator() % 10;
             }
         }
         doubleMatrix = matrix;
@@ -24,10 +43,12 @@ public:
 
     explicit DoubleMatrix(std::vector<std::vector<double>> temp) {
         doubleMatrix = std::move(temp);
+        rows = doubleMatrix.size();
+        columns = doubleMatrix[0].size();
     }
 
-    friend std::ostream& operator<< (std::ostream &out, const DoubleMatrix &dm) {
-        for (const std::vector<double>& i : dm.doubleMatrix) {
+    friend std::ostream &operator<<(std::ostream &out, const DoubleMatrix &dm) {
+        for (const std::vector<double> &i : dm.doubleMatrix) {
             for (double j : i) {
                 out << std::setw(5) << j << " ";
             }
@@ -36,8 +57,73 @@ public:
         return out;
     }
 
+    double determinant() {
+        const double EPS = 1E-9;
+        double det = 1;
+        if (rows != columns) {
+            throw MatrixException("The determinant cannot be calculated. Rows and columns of th matrix are not equal.");
+        }
+        for (int i = 0; i < rows; i++) {
+            int index = i;
+            for (int j = i + 1; j < rows; j++) {
+                if (std::abs(doubleMatrix[j][i]) > std::abs(doubleMatrix[index][i]))
+                    index = j;
+            }
+            if (std::abs(doubleMatrix[index][i]) < EPS) {
+                det = 0;
+                break;
+            }
+            swap(doubleMatrix[i], doubleMatrix[index]);
+            if (i != index) {
+                det = -det;
+            }
+            det *= doubleMatrix[i][i];
+            for (int j = i + 1; j < rows; j++) {
+                doubleMatrix[i][j] /= doubleMatrix[i][i];
+            }
+            for (int j = 0; j < rows; j++) {
+                if (j != i && std::abs(doubleMatrix[j][i]) > EPS) {
+                    for (int k = i + 1; k < rows; k++) {
+                        doubleMatrix[j][k] -= doubleMatrix[i][k] * doubleMatrix[j][i];
+                    }
+                }
+            }
+        }
+        return det;
+    }
+
+    double normM() {
+        double norm = 0;
+        double temp = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                temp += std::abs(doubleMatrix[i][j]);
+            }
+            if (temp >= norm) {
+                norm = temp;
+            }
+            temp = 0;
+        }
+        return norm;
+    }
+
+    double normL() {
+        double norm = 0;
+        double temp = 0;
+        for (int j = 0; j < columns; j++) {
+            for (int i = 0; i < rows; i++) {
+                temp += std::abs(doubleMatrix[i][j]);
+            }
+            if (temp >= norm) {
+                norm = temp;
+            }
+            temp = 0;
+        }
+        return norm;
+    }
+
     template<typename T>
-    friend DoubleMatrix operator* (DoubleMatrix &dm, T value) {
+    friend DoubleMatrix operator*(DoubleMatrix &dm, T value) {
         for (int i = 0; i < dm.doubleMatrix.size(); i++) {
             for (int j = 0; j < dm.doubleMatrix[0].size(); j++) {
                 dm.doubleMatrix[i][j] *= value;
@@ -47,7 +133,7 @@ public:
     }
 
     template<typename T>
-    friend void operator*= (DoubleMatrix &dm1, T value) {
+    friend void operator*=(DoubleMatrix &dm1, T value) {
         for (int i = 0; i < dm1.doubleMatrix.size(); i++) {
             for (int j = 0; j < dm1.doubleMatrix[0].size(); j++) {
                 dm1.doubleMatrix[i][j] *= value;
@@ -55,28 +141,34 @@ public:
         }
     }
 
-    friend DoubleMatrix operator* (DoubleMatrix &dm1, DoubleMatrix &dm2) {
-        int i_value = dm1.doubleMatrix.size();
-        int j_value = dm2.doubleMatrix[0].size();
-        int k_value = dm1.doubleMatrix[0].size();
+    friend DoubleMatrix operator*(DoubleMatrix &dm1, DoubleMatrix &dm2) {
+        if (dm1.columns != dm2.rows) {
+            throw MatrixException("Invalid operation. Boundary sizes of matrices are not equal.");
+        }
+        int i_value = dm1.rows;
+        int j_value = dm2.columns;
+        int k_value = dm1.columns;
         double temp = 0;
         std::vector<std::vector<double>> result(i_value, std::vector<double>(j_value));
-            for (int i = 0; i < i_value; i++) {
-                for (int j = 0; j < j_value; j++) {
-                    for (int k = 0; k < k_value; k++) {
-                        temp += dm1.doubleMatrix[i][k] * dm2.doubleMatrix[k][j];
-                    }
-                    result[i][j] = temp;
-                    temp = 0;
+        for (int i = 0; i < i_value; i++) {
+            for (int j = 0; j < j_value; j++) {
+                for (int k = 0; k < k_value; k++) {
+                    temp += dm1.doubleMatrix[i][k] * dm2.doubleMatrix[k][j];
                 }
+                result[i][j] = temp;
+                temp = 0;
             }
+        }
         return DoubleMatrix(result);
     }
 
-    friend void operator*= (DoubleMatrix &dm1, DoubleMatrix &dm2) {
-        int i_value = dm1.doubleMatrix.size();
-        int j_value = dm2.doubleMatrix[0].size();
-        int k_value = dm1.doubleMatrix[0].size();
+    friend void operator*=(DoubleMatrix &dm1, DoubleMatrix &dm2) {
+        if (dm1.columns != dm2.rows) {
+            throw MatrixException("Invalid operation. Boundary sizes of matrices are not equal.");
+        }
+        int i_value = dm1.rows;
+        int j_value = dm2.columns;
+        int k_value = dm1.columns;
         double temp = 0;
         std::vector<std::vector<double>> result(i_value, std::vector<double>(j_value));
         for (int i = 0; i < i_value; i++) {
@@ -90,19 +182,33 @@ public:
         }
         dm1.doubleMatrix = result;
     }
+
+    std::vector<double> operator[](int index) {
+        return doubleMatrix[index];
+    }
 };
 
 
-int main()
-{
-    DoubleMatrix matrix1 = DoubleMatrix(5, 6);
-    DoubleMatrix matrix2 = DoubleMatrix(6, 4);
-    std::cout << "Matrix1\n" << matrix1;
-    std::cout << "Matrix2\n" << matrix2;
-    std::cout << "Matrix1 * Matrix2\n" << matrix1 * matrix2;
-    matrix1 *= matrix2;
-    std::cout << "Matrix1\n" << matrix1;
-    matrix2 *= 2;
-    std::cout << "Matrix2\n" << matrix2;
+int main() {
+    try {
+        DoubleMatrix matrix1 = DoubleMatrix(6, 5);
+        DoubleMatrix matrix2 = DoubleMatrix(5, 4);
+        DoubleMatrix matrix3 = DoubleMatrix(5, 5);
+        std::cout << "Matrix1\n" << matrix1;
+        std::cout << "Matrix2\n" << matrix2;
+        std::cout << "Matrix1 * Matrix2\n" << matrix1 * matrix2;
+        matrix1 *= matrix2;
+        std::cout << "Matrix1\n" << matrix1;
+        matrix2 *= 2;
+        std::cout << "Matrix2\n" << matrix2;
+        std::cout << matrix1[1][1] << std::endl;
+        std::cout << matrix2.normM() << std::endl;
+        std::cout << matrix2.normL() << std::endl;
+        std::cout << matrix3 << std::endl;
+        std::cout << matrix3.determinant() << std::endl;
+    }
+    catch (MatrixException &exception) {
+        std::cout << exception.what() << std::endl;
+    }
 
 }
