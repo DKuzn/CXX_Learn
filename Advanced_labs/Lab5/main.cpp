04499470ead6779b870c2bcdf7290e0c4f361e5b
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-10-30 18:31:55 +0300

    update 5th lab

--- Advanced_labs/Lab5/main.cpp
+++ Advanced_labs/Lab5/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include "BaseString.h"
+#include "DecimalString.h"
 
 int main() {
-    BaseString str = BaseString('&');
-    std::cout << str.value << std::endl;
+    DecimalString str1 = DecimalString("+50");
+    DecimalString str2 = DecimalString("-3");
+    DecimalString str3 = DecimalString("234");
+    std::cout << "Decimal string 1:\n" << str1 << std::endl;
+    std::cout << "Decimal string 2:\n" << str2 << std::endl;
+    std::cout << "Decimal string 3:\n" << str3 << std::endl;
+    std::cout << "String3 - String1:\n" << (str3 - str1) << std::endl;
+    std::cout << "String1 > String2:\n" << (str1 > str2) << std::endl;
+    std::cout << "String1 < String2:\n" << (str1 < str2) << std::endl;
 }
